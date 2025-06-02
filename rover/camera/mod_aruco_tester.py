137a5969979b75d989c8a740cc128678684a41e3
ShawarmaDog <77565043+ShawarmaDog@users.noreply.github.com>
2025-06-02 09:48:15 +0800

    updated mod_aruco_tester
    prints only every second
    and made into 720p

--- rover/camera/mod_aruco_tester.py
+++ rover/camera/mod_aruco_tester.py
@@ -1,5 +1,5 @@
 import cv2
-import cv2.aruco as aruco
+import cv2.aruco as aruco # type: ignore
 import numpy as np
 from imutils.video import WebcamVideoStream
 import imutils
@@ -7,8 +7,8 @@ import time
 
 #############################
 
-width = 640
-height = 480
+width = 1280
+height = 720
 cap = WebcamVideoStream(src=0, height=height, width=width).start()
 
 ############ARUCO/CV2############
@@ -27,6 +27,46 @@ seconds = 1000000  # Run indefinitely until manually stopped
 counter = 0
 
 start_time = time.time()
+last_print_time = time.time()  # Track the last time text was printed
+
+while time.time() - start_time < seconds:
+    frame = cap.read()  # For Threaded webcam
+    frame_np = np.array(frame)
+    gray_img = cv2.cvtColor(frame_np, cv2.COLOR_BGR2GRAY)
+    ids = ''
+    corners, ids, rejected = aruco.detectMarkers(image=gray_img, dictionary=aruco_dict, parameters=parameters)
+
+    if ids is not None:
+        if ids[0] == id_to_find:
+            ret = aruco.estimatePoseSingleMarkers(corners, marker_size, cameraMatrix=cameraMatrix, distCoeffs=cameraDistortion)
+            rvec, tvec = ret[0][0, 0, :], ret[1][0, 0, :]
+            x = "{:.2f}".format(tvec[0])
+            y = "{:.2f}".format(tvec[1])
+            z = "{:.2f}".format(tvec[2])
+            marker_position = "MARKER POSITION: x=" + x + " y=" + y + " z=" + z
+
+            # Print text only if 1 second has passed since the last print
+            if time.time() - last_print_time >= 1:
+                print(marker_position)
+                print("")
+                last_print_time = time.time()
+
+            aruco.drawDetectedMarkers(frame_np, corners)
+            aruco.drawAxis(frame_np, cameraMatrix, cameraDistortion, rvec, tvec, 10)
+    else:
+        if time.time() - last_print_time >= 1:
+            print("ARUCO " + str(id_to_find) + " NOT FOUND IN FRAME.")
+            print("")
+            last_print_time = time.time()
+
+    cv2.imshow('frame', frame_np)
+    if cv2.waitKey(1) & 0xFF == ord('q'):  # Press 'q' to exit
+        break
+    counter += 1
+
+cv2.destroyAllWindows()
+''' print every split second
+start_time = time.time()
 while time.time() - start_time < seconds:
     frame = cap.read()  # For Threaded webcam
     frame_np = np.array(frame)
@@ -36,18 +76,19 @@ while time.time() - start_time < seconds:
     if ids is not None:
         print("Found these IDs in the frame:")
         print(ids)
-    if ids is not None and ids[0] == id_to_find:
-        ret = aruco.estimatePoseSingleMarkers(corners, marker_size, cameraMatrix=cameraMatrix, distCoeffs=cameraDistortion)
-        rvec, tvec = ret[0][0, 0, :], ret[1][0, 0, :]
-        x = "{:.2f}".format(tvec[0])
-        y = "{:.2f}".format(tvec[1])
-        z = "{:.2f}".format(tvec[2])
-        marker_position = "MARKER POSITION: x=" + x + " y=" + y + " z=" + z
-        print(marker_position)
-        print("")
-        aruco.drawDetectedMarkers(frame_np, corners)
-        aruco.drawAxis(frame_np, cameraMatrix, cameraDistortion, rvec, tvec, 10)
-        #this one breaks the program for some reason. drawAxis not found
+
+        if ids is not None and ids[0] == id_to_find:
+            ret = aruco.estimatePoseSingleMarkers(corners, marker_size, cameraMatrix=cameraMatrix, distCoeffs=cameraDistortion)
+            rvec, tvec = ret[0][0, 0, :], ret[1][0, 0, :]
+            x = "{:.2f}".format(tvec[0])
+            y = "{:.2f}".format(tvec[1])
+            z = "{:.2f}".format(tvec[2])
+            marker_position = "MARKER POSITION: x=" + x + " y=" + y + " z=" + z
+            print(marker_position)
+            print("")
+            aruco.drawDetectedMarkers(frame_np, corners)
+            aruco.drawAxis(frame_np, cameraMatrix, cameraDistortion, rvec, tvec, 10)
+            #this one breaks the program for some reason. drawAxis not found
     else:
         print("ARUCO " + str(id_to_find) + " NOT FOUND IN FRAME.")
         print("")
@@ -55,5 +96,6 @@ while time.time() - start_time < seconds:
     if cv2.waitKey(1) & 0xFF == ord('q'):  # Press 'q' to exit
         break
     counter += 1
-
 cv2.destroyAllWindows()
+    '''
+
